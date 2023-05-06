b43775b6475e78bb316117d65215eb2db95f341a
Márkus Martin <markus.martin@unixauto.hu>
2023-05-06 22:08:39 +0200

    w

--- src/doc-ext/scripts/content_script.js
+++ src/doc-ext/scripts/content_script.js
@@ -1,13 +1,45 @@
 
 readHtmlAsync('doc-ext/templates/content_template.html')
   .then(doc => {
+    console.log('asdasd');
     document.body.appendChild(htmlToElems(doc));
+    setHeaderLogo();
   });
 
+initMoveListener();
 
 /*
   Utils
 */
+
+let trackMouse = false;
+
+function initMoveListener() {
+  const header =  document.getElementById('doc-ext-header-A8A662C82DA3');
+
+  header.addEventListener('mousedown', () => {
+    console.log("down");
+    trackMouse = true;
+  });
+  document.addEventListener('mouseup', () => {
+    console.log("up");
+    trackMouse = false;
+  });
+  document.addEventListener('mousemove', () => {
+    if (trackMouse === true) {
+      const body = document.querySelector('.body-A8A662C82DA3');
+      body.left += 1;
+    }
+  });
+
+}
+
+function setHeaderLogo() {
+  const logo = document.querySelector('#doc-ext-header-icon-A8A662C82DA3');
+  logo.src = chrome.runtime.getURL("doc-ext/assets/robot.png");
+  logo.height = 20;
+  logo.width = 20;
+}
 
 function readHtmlAsync(filePath) {
   return fetch(chrome.runtime.getURL(filePath))
