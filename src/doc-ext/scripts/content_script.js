95c1e30ba3f7937d3c95e7e99569ac348dc9c43c
Márkus Martin <martinmarkus0@gmail.com>
2023-05-09 21:59:52 +0200

    wip

--- src/doc-ext/scripts/content_script.js
+++ src/doc-ext/scripts/content_script.js
@@ -1,11 +1,12 @@
 readHtmlAsync('doc-ext/templates/content_template.html')
 .then(doc => {
-  console.log(doc);
-  // INFO: First append the markup
-  console.log(JSON.stringify(htmlToElems(doc)));
-  document.body.appendChild(htmlToElems(doc));
-  //setHeaderLogo();
-  //initMoveListener();
+  // INFO: First append the markup;
+  widget = htmlToElems(doc);
+  document.body.appendChild(widget);
+  setHeaderLogo();
+  setGptLogo();
+  setUserLogo();
+  initMoveListener();
 
 });
 
@@ -13,34 +14,67 @@ readHtmlAsync('doc-ext/templates/content_template.html')
   Utils
 */
 
-// let trackMouse = false;
+let widget;
+
+let trackMouse = false;
 
 // function initMoveListener() {
-//   const header = document.body.querySelector('#doc-ext-header-A8A662C82DA3');
+//   const header = widget.querySelector('#doc-ext-header-A8A662C82DA3');
 
 //   header.addEventListener('mousedown', () => {
 //     console.log("down");
 //     trackMouse = true;
 //   });
-//   document.body.addEventListener('mouseup', () => {
+
+//   widget.addEventListener('mouseup', () => {
 //     console.log("up");
 //     trackMouse = false;
 //   });
-//   document.body.addEventListener('mousemove', () => {
+
+//   widget.addEventListener('mousemove', () => {
 //     if (trackMouse === true) {
-//       const body = document.body.querySelector('.body-A8A662C82DA3');
-//       body.left += 1;
+//       console.log('widget: ' + widget);
+
+//       console.log(widget.style.left);
+//       widget.style.left = '0';
 //     }
 //   });
-
 // }
 
 function setHeaderLogo() {
-  const logo = document.body.querySelector('#doc-ext-header-icon-A8A662C82DA3');
+  const logo = widget.querySelector('#doc-ext-header-icon-A8A662C82DA3');
   logo.src = chrome.runtime.getURL("doc-ext/assets/robot.png");
   logo.height = 20;
   logo.width = 20;
 }
+
+function setGptLogo() {
+  const logos = widget.querySelectorAll('.doc-ext-gtp-icon-A8A662C82DA3');
+
+  logos.forEach((x) => {
+    x.src = chrome.runtime.getURL("doc-ext/assets/gpt-icon.png");
+    x.height = 32;
+    x.width = 32;
+  })
+}
+
+function setUserLogo() {
+  const logos = widget.querySelectorAll('.doc-ext-person-A8A662C82DA3');
+
+  logos.forEach((x) => {
+    x.src = chrome.runtime.getURL("doc-ext/assets/person-outline.png");
+    x.height = 32;
+    x.width = 32;
+  })
+}
+
+function setHeaderLogo() {
+  const logo = widget.querySelector('#doc-ext-header-icon-A8A662C82DA3');
+  logo.src = chrome.runtime.getURL("doc-ext/assets/robot.png");
+  logo.height = 20;
+  logo.width = 20;
+}
+
 
 function readHtmlAsync(filePath) {
   return fetch(chrome.runtime.getURL(filePath))
